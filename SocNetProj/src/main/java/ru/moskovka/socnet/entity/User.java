8168a949a85ddc54346491518072a6ffee2df504
Natalia Moskovka <nat.chan.des@yandex.ru>
2022-03-01 11:00:07 +0300

    1. Added missing fields to User \n2. Started implementing fields valudation

--- SocNetProj/src/main/java/ru/moskovka/socnet/entity/User.java
+++ SocNetProj/src/main/java/ru/moskovka/socnet/entity/User.java
@@ -1,12 +1,68 @@
 package ru.moskovka.socnet.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "User")
 public class User {
     @Id
     private String login;
+
+    @Column
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @Column
+    @NotEmpty(message = "Surname should not be empty")
+    private String surname;
+
+    @Column
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
